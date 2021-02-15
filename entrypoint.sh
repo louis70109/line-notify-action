1db674d93b13f0e226450ef36705e802cb0fb483
louis70109 <louis70109@gmail.com>
2021-02-16 01:48:02 +0800

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,16 @@
 if [ $# -ne 0 ]; then
     exec /notify_irc.py "$@"
 fi
-
+if [ "$INPUT_TLS" == "true" ]; then
+    ARG_TLS="--tls"
+else
+    ARG_TLS=""
+fi
+if [ "$INPUT_NOTICE" == "true" ]; then
+    ARG_NOTICE="--notice"
+else
+    ARG_NOTICE=""
+fi
 exec /line_notify.py \
   --token "$INPUT_TOKEN" \
   --message "$INPUT_MESSAGE" \
